cfd02cede63cf66fd32dbede12c28b855d5b27e3
Chris Flores <florescss14@gmail.com>
2021-03-02 02:58:27 +0000

    Basic html and css
    Added a stylesheet and some basic pages to the website. Linked the _base.html template as well too

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
-    
+    return render_template("index.html")
+
+@app.route('/Profile')
+def profile_view():
+    return render_template("profile.html")
