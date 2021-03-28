e1be4a1916587e861577db3ebe83525ab6bc5ebe
Chris Flores <florescss14@gmail.com>
2021-03-28 18:36:09 +0000

    Added join_group function

--- groups.py
+++ groups.py
@@ -1,3 +1,4 @@
+import json
 # Imports the Google Cloud client library
 from google.cloud import datastore
 # Instantiates a client
@@ -29,22 +30,38 @@ def create_group(group_name, max_size, password, username):
 
     # Saves the entity
     datastore_client.put(group)
-    join_group(group_name,username)
+    join_group(group_name,username, password)
     return True
 
-def join_group(group_name, username):
+def join_group2(group_name, username):
     # Have to check if the group actually exists and if the user is allowed to join
-    datastore_client
-    # The Cloud Datastore key for the new entity
-    group_key = datastore_client.key("Group_Members")
+    query = datastore_client.query(kind="Groups")
+    query.add_filter("group_name", "=", group_name)
+    groups = list(query.fetch())   #retrieves and puts entities in a list 
+    print(groups[0]["owner"])
 
-    # Prepares the new entity
-    group_mem = datastore.Entity(key=group_key)
-    # Put attrubutes from the group in this spot
-    group_mem["group_name"] = group_name
-    group_mem["username"] = username
+def join_group(group_name, username, password):
+    # Have to check if the group actually exists and if the user is allowed to join
+    query = datastore_client.query(kind="Groups")
+    query.add_filter("group_name", "=", group_name)
+    groups = list(query.fetch())
+    if (groups[0]["max_size"] > groups[0]["group_size"]):
+        if password == groups[0]["password"]:
+            # The Cloud Datastore key for the new entity
+            group_key = datastore_client.key("Group_Members")
 
-    # Saves the entity
-    datastore_client.put(group_mem)
+            # Prepares the new entity
+            group_mem = datastore.Entity(key=group_key)
+            # Put attrubutes from the group in this spot
+            group_mem["group_name"] = group_name
+            group_mem["username"] = username
+
+            # Saves the entity
+            datastore_client.put(group_mem)
+        else:  #failed because of incorrect password
+            print("Failed to Join: Incorrect password")
+    else:
+        print("Failed to Join: Group size error")
+        #failed to join because of group size
 
 
