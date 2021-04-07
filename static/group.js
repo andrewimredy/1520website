f1ec34932e851dd600e055776c137e5423a54f46
Chris Flores <florescss14@gmail.com>
2021-04-07 02:19:12 +0000

    added some css to prep for javascript

---
+++ static/group.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+    /*
+    const form = document.getElementById("question-form");
+    form.addEventListener("submit", submitQuestion);
+    loadQuestions();
+    */
+    console.log("Hello");
+    // executes function every x milliseconds
+    //window.setInterval(loadQuestions, 30000);
+}
+
+function ascendSortUsernames(){
+    
+}
