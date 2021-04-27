7468dc3f1674b91f13916b84cdc9dceffb2c25e1
Andrew Imredy <andrew.imredy@gmail.com>
2021-04-27 21:26:45 +0000

    html ids, js file

--- static/andrews_js.js
+++ static/andrews_js.js
@@ -1,5 +1,15 @@
-//change time heading to "Bet Placed"
 
-function placeBet(){    
-    document.getElementById().innerHTML = "Bet Placed"
+
+function placeBet(teamAndGame){    
+    //gameId comes in as 'x000000000' to show bet pref and id. lets parse it
+    let team = teamAndGame[0];
+    let gameId = teamAndGame.substring(1);
+    
+    //if no user is logged in, alert
+    if(document.getElementById('username') === null){
+        alert('Please log in to place bets!');
+        return;
+    }
+    //change time heading to "Bet Placed"
+    document.getElementById('t' + gameId).innerHTML = 'Bet Placed';
 }
